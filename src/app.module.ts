e81a8b1c7ff1d6f6a7a2060838280bb2790c6f90
IliaPonomarev <seikadi123@gmail.com>
2023-05-14 17:34:13 +0300

    Init with nestjs boilerplate

---
+++ src/app.module.ts
@@ -0,0 +1,48 @@
+import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { FilesModule } from './files/files.module';
+import { AuthModule } from './auth/auth.module';
+import databaseConfig from './config/database.config';
+import authConfig from './config/auth.config';
+import appConfig from './config/app.config';
+import mailConfig from './config/mail.config';
+import fileConfig from './config/file.config';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './database/typeorm-config.service';
+import { MailConfigService } from './mail/mail-config.service';
+import { ForgotModule } from './forgot/forgot.module';
+import { MailModule } from './mail/mail.module';
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [
+        databaseConfig,
+        authConfig,
+        appConfig,
+        mailConfig,
+        fileConfig
+      ],
+      envFilePath: ['.env'],
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+      dataSourceFactory: async (options: DataSourceOptions) => {
+        return new DataSource(options).initialize();
+      },
+    }),
+    MailerModule.forRootAsync({
+      useClass: MailConfigService,
+    }),
+    UsersModule,
+    FilesModule,
+    AuthModule,
+    ForgotModule,
+    MailModule
+  ],
+})
+export class AppModule {}
